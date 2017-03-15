ce6a386ae24256d4b81e6cc458625edbd85f74f3
cole-thompson <colethompson@utexas.edu>
2017-03-15 15:35:48 -0500

    created module for filling VAO buffers from .obj

--- ModelTextureMovement/src/main.cpp
+++ ModelTextureMovement/src/main.cpp
@@ -26,32 +26,7 @@ void window_size_callback(GLFWwindow* window, int width, int height) {
 	windowW = width;
 	//glfwSetCursorPos(window, windowW / 2, windowH / 2);
 }
-
-
-//draws the triangles in the vertex buffer with the UV's, uses shaders
-void drawTriangles(GLuint* vertexbuffer, GLuint* uvbuffer, int numVertices) {
-	//attribute buffer 1 for vertices
-	glEnableVertexAttribArray(0);
-	glBindBuffer(GL_ARRAY_BUFFER, *vertexbuffer);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
-
-	//attribute buffer 2 for UV's
-	glEnableVertexAttribArray(1);
-	glBindBuffer(GL_ARRAY_BUFFER, *uvbuffer);
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
-
-	glDrawArrays(GL_TRIANGLES, 0, numVertices);
-	glDisableVertexAttribArray(0);
-	glDisableVertexAttribArray(1);
-}
-
-glm::mat4 getModelMatrix() {
-	//translation*rotation*scale matrices for model
-	return glm::mat4(1.0);
-}
-
-//MAIN
-int main() {
+int initializeWindow() {
 	if (!glfwInit()) {
 		fprintf(stderr, "GLFW not initializing");
 		return -1;
@@ -65,8 +40,8 @@ int main() {
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);	//no old OpenGL
 	glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);	//reszeable window
 
-	//create GLFWwindow and OpenGL context
-	//for full screen, make the monitor parameter glfwGetPrimaryMonitor()
+												//create GLFWwindow and OpenGL context
+												//for full screen, make the monitor parameter glfwGetPrimaryMonitor()
 	windowW = 1024;
 	windowH = 768;
 	window = glfwCreateWindow(windowW, windowH, ".OBJ Model, .DDS Texture, and User Movement", NULL, NULL);
@@ -77,8 +52,8 @@ int main() {
 	}
 
 	//make window current context
-	glfwMakeContextCurrent(window);		
-	
+	glfwMakeContextCurrent(window);
+
 	//initialize GLEW
 	glewExperimental = true;
 	if (glewInit() != GLEW_OK) {
@@ -108,66 +83,101 @@ int main() {
 
 	//background
 	glClearColor(0.0f, 0.2f, 0.0f, 0.0f);
-	// Enable depth test
+}
+
+//returns size, or -1 if object didn't load
+int loadObjectBuffers(const char* objFilePath, GLuint *vertexArrayObject, GLuint * objVertexbuffer, GLuint * objUVbuffer) {
+	glGenVertexArrays(1, vertexArrayObject);
+	glBindVertexArray(*vertexArrayObject);
+	
+	std::vector<glm::vec3> objVertices;
+	std::vector<glm::vec2> objUvs;
+	std::vector<glm::vec3> objNormals;
+	if (!loadObjFile(objFilePath, &objVertices, &objUvs, &objNormals)) {
+		return -1;
+	}
+
+	//vertex buffer object openGL VBO setup
+	glGenBuffers(1, objVertexbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, *objVertexbuffer);
+	glBufferData(GL_ARRAY_BUFFER, (objVertices.size() * sizeof(glm::vec3)), &objVertices[0], GL_STATIC_DRAW);
+
+	//uv buffer setup
+	glGenBuffers(1, objUVbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, *objUVbuffer);
+	glBufferData(GL_ARRAY_BUFFER, (objUvs.size() * sizeof(glm::vec2)), &objUvs[0], GL_STATIC_DRAW);
+
+	return objVertices.size();
+}
+
+//draws the triangles in the vertex buffer with the UV's, uses shaders
+void drawTriangles(GLuint* vertexbuffer, GLuint* uvbuffer, int numVertices) {
+	//attribute buffer 1 for vertices
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, *vertexbuffer);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+
+	//attribute buffer 2 for UV's
+	glEnableVertexAttribArray(1);
+	glBindBuffer(GL_ARRAY_BUFFER, *uvbuffer);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
+
+	glDrawArrays(GL_TRIANGLES, 0, numVertices);
+	glDisableVertexAttribArray(0);
+	glDisableVertexAttribArray(1);
+}
+
+glm::mat4 getModelMatrix() {
+	//translation*rotation*scale matrices for model
+	return glm::mat4(1.0);
+}
+
+//MAIN
+int main() {
+	if (initializeWindow() == -1) {
+		return -1;
+	}
+	
+	//acccepts fragment closer to camera
 	glEnable(GL_DEPTH_TEST);
-	// Accept fragment if it closer to the camera than the former one
 	glDepthFunc(GL_LESS);
 	//No triangles who's normals don't face the camera
 	//glEnable(GL_CULL_FACE);
 
 
-	//vertex array object openGL VAO
-	GLuint vertexArray;
-	glGenVertexArrays(1, &vertexArray);
-	glBindVertexArray(vertexArray);
-
 	//create shaders from files with shaders.cpp
 	GLuint shaderProgram = loadShaders("recources/shaders/mvpVertexShaderUV.vert", "recources/shaders/textureFragmentShader.frag");
 
 	// MVP uniform variable. these are used to communicate with shader. MVP in vertexShader
-	GLuint MVPMatrixID = glGetUniformLocation(shaderProgram, "MVP");
+	GLuint mvpMatrixUniform = glGetUniformLocation(shaderProgram, "MVP");
 	
-	//create textures frrom files with textureInput.cpp
+	//create textures from files with textureInput.cpp
 	GLuint Texture = loadDDSTexture("recources/textures/checkers.DDS");
 	//mySampler2D uniform variable 
-	GLuint TextureID = glGetUniformLocation(shaderProgram, "mySampler2D");
-
-	std::vector<glm::vec3> objVertices;
-	std::vector<glm::vec2> objUvs;
-	std::vector<glm::vec3> objNormals;
-	bool isObjLoaded = loadObjFile("recources/objects/monkey.obj", &objVertices, &objUvs, &objNormals);
-
-	//vertex buffer object openGL VBO setup
-	GLuint cubeVertexbuffer;
-	glGenBuffers(1, &cubeVertexbuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, cubeVertexbuffer);
-	glBufferData(GL_ARRAY_BUFFER, (objVertices.size() * sizeof(glm::vec3)), &objVertices[0], GL_STATIC_DRAW);
-
-	//uv buffer setup
-	GLuint cubeUVbuffer;
-	glGenBuffers(1, &cubeUVbuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, cubeUVbuffer);
-	glBufferData(GL_ARRAY_BUFFER, (objUvs.size() * sizeof(glm::vec2)), &objUvs[0], GL_STATIC_DRAW);
+	GLuint textureUniform = glGetUniformLocation(shaderProgram, "mySampler2D");
 	
-	while (!glfwWindowShouldClose(window) && glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS) {
-		//clear screen
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	//create a vertex array object out of a .obj file and bind it, add two attribute buffers for verticies and uv's
+	//UV coordinates correspond to Texture
+	GLuint vertexArrayObject, objVertexbuffer, objUVbuffer;
+	int monkeySize = loadObjectBuffers("recources/objects/monkey.obj", &vertexArrayObject, &objVertexbuffer, &objUVbuffer);
+	if (monkeySize == -1) {
+		return -1;
+	}
 
-		//use shader
-		glUseProgram(shaderProgram);
+	while (!glfwWindowShouldClose(window) && glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS) {
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);	//clear screen
+
+		glUseProgram(shaderProgram);	//use shader
 
 		computeMVPMatrices(window);
-		glm::mat4 Projection = getPMatrix();
-		glm::mat4 View = getVMatrix();
-		glm::mat4 Model = getModelMatrix();
-		glm::mat4 MVP = Projection * View * Model;
-		glUniformMatrix4fv(MVPMatrixID, 1, GL_FALSE, &MVP[0][0]);	//link MVP matrix to uniform variable for shader
+		glm::mat4 MVP = getPMatrix() * getVMatrix() * getModelMatrix();
+		glUniformMatrix4fv(mvpMatrixUniform, 1, GL_FALSE, &MVP[0][0]);	//link MVP matrix to uniform variable for shader
 		
 		glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_2D, Texture);
-		glUniform1i(TextureID, 0);
+		glUniform1i(textureUniform, 0);
 
-		drawTriangles(&cubeVertexbuffer, &cubeUVbuffer, objVertices.size());
+		drawTriangles(&objVertexbuffer, &objUVbuffer, monkeySize);
 
 		//front and back buffers
 		glfwSwapBuffers(window);
@@ -176,11 +186,11 @@ int main() {
 		glfwPollEvents();
 	}
 
-	glDeleteBuffers(1, &cubeVertexbuffer);
-	glDeleteBuffers(1, &cubeUVbuffer);
+	glDeleteBuffers(1, &objVertexbuffer);
+	glDeleteBuffers(1, &objUVbuffer);
 	glDeleteProgram(shaderProgram);
-	glDeleteTextures(1, &TextureID);
-	glDeleteVertexArrays(1, &vertexArray);
+	glDeleteTextures(1, &textureUniform);
+	glDeleteVertexArrays(1, &vertexArrayObject);
 
 	//close window, end program
 	glfwDestroyWindow(window);
